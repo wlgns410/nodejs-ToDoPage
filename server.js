1fcd5717337105ceaafc1a17c2383fa35fa6ea69
wlgns410 <wlgns410@gmail.com>
2021-08-22 12:08:39 +0900

    ADD : setting mongodb

--- server.js
+++ server.js
@@ -4,11 +4,39 @@ const app = express();
 const bodyParser= require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.urlencoded({extended: true}));
+const MongoClient = require('mongodb').MongoClient;
 
-// 로컬 컴퓨터에 서버를 염(포트번호, 할 기능)
-app.listen(8080, function(){
-    console.log('listening on 8080')
-});
+// 어떤 데이터 베이스에 저장할 것인지
+var db;
+MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.czsgs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', function(err, client){
+    // 에러처리
+    if (err) return console.log(err);
+
+    // db 이름('todoapp')
+    db = client.db('todoapp');
+
+    // db 내에 있는 '폴더'(post 폴더)에 저장할 데이터를 삽입
+    db.collection('post').insertOne({name:'jihoon', age:27, _id:100}, function(err, result){
+        console.log('저장완료');
+    });
+
+    // 로컬 컴퓨터에 서버를 염(포트번호, 할 기능)
+    app.listen(8080, function(){
+        console.log('listening on 8080')
+    });
+
+    db.collection('post').insertOne({title:'운동', data:'2021-08-22', _id:1}, function(err, result){
+        console.log('일정 등록')
+    })
+
+    app.post('/add', function(req, res){
+        res.send('저장 완료');
+        console.log(req.body.date);
+        console.log(req.body.title);
+    })
+})
+
+
 
 // 누군가가 /pet으로 방문 시 pet 안내문 띄우기
 app.get('/pet', function(req, res){
@@ -34,8 +62,3 @@ app.get('/write', function(req, res){
 //     res.sendFile(__dirname + '/write.html')
 // })
 
-app.post('/add', function(req, res){
-    res.send('저장 완료');
-    console.log(req.body.date);
-    console.log(req.body.title);
-})
