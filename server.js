e40e3bec8487f757a4cea94e6f59bd4fd646cbf6
wlgns410 <wlgns410@gmail.com>
2021-08-22 17:09:35 +0900

    ADD : DB Update function,  inc operator

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser= require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.urlencoded({extended: true}));
 const MongoClient = require('mongodb').MongoClient;
+app.set('view engine', 'ejs');
 
 // 어떤 데이터 베이스에 저장할 것인지
 var db;
@@ -25,17 +26,34 @@ MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.czsgs.mongodb.net/myF
         console.log('listening on 8080')
     });
 
-    db.collection('post').insertOne({title:'운동', data:'2021-08-22', _id:1}, function(err, result){
-        console.log('일정 등록')
-    })
+});
 
-    app.post('/add', function(req, res){
-        res.send('저장 완료');
-        console.log(req.body.date);
-        console.log(req.body.title);
-    })
-})
+app.post('/add', function(req, res){
+    res.send('저장 완료');
+    console.log(req.body.date);
+    console.log(req.body.title);
 
+    // db.counter 내의 allpost를 찾음
+    db.collection('counter').findOne({ name : "게시물 개수" }, function(err, result){
+        console.log(result.totalPost)
+        var allPost = result.totalPost;
+
+        //db.post에서 가져옴
+        db.collection('post').insertOne({title:req.body.title, date:req.body.date, _id: allPost + 1}, function(err, result){
+            console.log('일정 등록')
+        
+            // 콜백함수 안에 작성 1개만 저장 $set, $inc 은 연산자 operator임 이건 따로 공부, totalpost + 1
+            db.collection('counter').updateOne({name: "게시물 개수"}, { $inc : {totalPost:1}}, function(err, result){
+                if(err) {return console.log(err)};
+            });
+        });
+        
+    });
+    
+});
+
+
+//list로 GET요청 시 실제 DB에 저장된 데이터들로 예쁘게 꾸며진HTML을 보여줌
 
 
 // 누군가가 /pet으로 방문 시 pet 안내문 띄우기
@@ -55,10 +73,19 @@ app.get('/', function(req, res){
 
 app.get('/write', function(req, res){
     res.sendFile(__dirname + '/write.html')
-})
+});
 
 // es6 문법
 // app.get('/write', (req, res) => {
 //     res.sendFile(__dirname + '/write.html')
 // })
 
+app.get('/list', function(req, res){
+    // find : 모든 데이터 가져옴
+    db.collection('post').find().toArray(function(err, result){
+        console.log(result);
+        res.render('list.ejs', { posts : result });
+    });
+});
+
+// 글 번호를 달아서 저장
